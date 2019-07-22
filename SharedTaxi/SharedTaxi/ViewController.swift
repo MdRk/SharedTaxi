d8ae0b69293ea8ccfe70c83c2db2f5b5bb9ba2d6
前田陸 <>
2019-07-22 14:38:36 +0900

    プロジェクト追加

---
+++ SharedTaxi/SharedTaxi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SharedTaxi
+//
+//  Created by 前田陸 on 2019/07/22.
+//  Copyright © 2019 陸　前田. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SharedTaxi/SharedTaxiTests/SharedTaxiTests.swift
@@ -0,0 +1,34 @@
+//
+//  SharedTaxiTests.swift
+//  SharedTaxiTests
+//
+//  Created by 前田陸 on 2019/07/22.
+//  Copyright © 2019 陸　前田. All rights reserved.
+//
+
+import XCTest
+@testable import SharedTaxi
+
+class SharedTaxiTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
