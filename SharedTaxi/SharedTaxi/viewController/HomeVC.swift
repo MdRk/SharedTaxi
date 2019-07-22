ddb5cbe5a34dc799907809120fa61bd82846f11c
前田陸 <>
2019-07-22 14:55:30 +0900

    初期ファイル追加

---
+++ SharedTaxi/SharedTaxi/viewController/HomeVC.swift
@@ -0,0 +1,17 @@
+//
+//  HomeVC.swift
+//  SharedTaxi
+//
+//  Created by 前田陸 on 2019/07/22.
+//  Copyright © 2019 陸　前田. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ SharedTaxi/SharedTaxi/viewController/SettingVC.swift
@@ -0,0 +1,30 @@
+//
+//  SettingVC.swift
+//  SharedTaxi
+//
+//  Created by 前田陸 on 2019/07/22.
+//  Copyright © 2019 陸　前田. All rights reserved.
+//
+
+import UIKit
+
+class SettingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
