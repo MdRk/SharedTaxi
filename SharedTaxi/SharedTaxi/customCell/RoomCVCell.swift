5b15a2b7be4c7a21cdf198a358991dddf5e2d46f
前田陸 <>
2019-07-23 17:36:35 +0900

    customCellを実装

---
+++ SharedTaxi/SharedTaxi/customCell/RoomCVCell.swift
@@ -0,0 +1,18 @@
+//
+//  RoomCVCell.swift
+//  SharedTaxi
+//
+//  Created by 前田陸 on 2019/07/23.
+//  Copyright © 2019 陸　前田. All rights reserved.
+//
+
+import UIKit
+
+class RoomCVCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
