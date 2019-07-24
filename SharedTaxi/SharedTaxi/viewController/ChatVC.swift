920c143be1ed3cb8b05ae3c46b94fde0e2941254
前田陸 <>
2019-07-24 09:22:40 +0900

    チャット画面のファイルと追加

---
+++ SharedTaxi/SharedTaxi/viewController/ChatVC.swift
@@ -0,0 +1,30 @@
+//
+//  ChatVC.swift
+//  SharedTaxi
+//
+//  Created by 前田陸 on 2019/07/24.
+//  Copyright © 2019 陸　前田. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
